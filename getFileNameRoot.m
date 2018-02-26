0fa78513dae37308eccd52b53d78a28f32d17b79
Clark Dunson <cdunson@gmail.com>
2018-02-26 15:41:43 -0800

    Added comment to getFileNameRoot.m; Finished runVariant.m

--- getFileNameRoot.m
+++ getFileNameRoot.m
@@ -3,9 +3,9 @@ function fNameRoot =  getFileNameRoot( v, comment )
 datm = datestr( now, 30 );
 tm = datestr( now, 13 );
 if nargin > 1
-    str = [ v{1}, '_', v{2}, '_', v{3}, '_', v{4}, '_', v{5}, '_', datm, '_', comment, '.flac' ];
+    str = [ v{1}, '_', v{2}, '_', v{3}, '_', v{4}, '_', v{5}, '_', datm, '_', comment ];
 else
-    str = [ v{1}, '_', v{2}, '_', v{3}, '_', v{4}, '_', v{5}, '_', datm, '.flac' ];
+    str = [ v{1}, '_', v{2}, '_', v{3}, '_', v{4}, '_', v{5}, '_', datm ];
 end
 
 fNameRoot = str;
--- runVariant.m
+++ runVariant.m
@@ -1,1 +1,14 @@
-function fNameRoot =  runVariant( v )
+function success =  runVariant( variant, comment )
+
+fName = [ getFileNameRoot( variant, comment ), '.flac' ];
+
+success = 0;
+if ~recordVariant( fName )
+	error('Could not record.')
+end
+
+if ~plotVariant( fName )
+	error('Could not plot.')
+end
+
+success = 1;
