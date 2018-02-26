1bb85410cf0ac3779915105744fbc0fcc56d7131
Clark Dunson <cdunson@gmail.com>
2018-02-26 15:29:47 -0800

    Finished getFileNameRoot.m

--- getFileNameRoot.m
+++ getFileNameRoot.m
@@ -1,5 +1,11 @@
-function fNameRoot =  getFileNameRoot( v )
+function fNameRoot =  getFileNameRoot( v, comment )
 
 datm = datestr( now, 30 );
 tm = datestr( now, 13 );
-str = [ v{1}, '_', v{2}, '_', v{3}, '_', v{4}, '_', v{5}, '_', datm, '.flac' ]
+if nargin > 1
+    str = [ v{1}, '_', v{2}, '_', v{3}, '_', v{4}, '_', v{5}, '_', datm, '_', comment, '.flac' ];
+else
+    str = [ v{1}, '_', v{2}, '_', v{3}, '_', v{4}, '_', v{5}, '_', datm, '.flac' ];
+end
+
+fNameRoot = str;
