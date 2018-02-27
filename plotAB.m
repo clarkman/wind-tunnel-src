35d866230e68e095b18e82fb1236f73debe89480
Clark Dunson <cdunson@gmail.com>
2018-02-27 06:29:12 -0800

    Completed pathwork

--- plotAB.m
+++ plotAB.m
@@ -1,32 +1,40 @@
-function idx =  plotAB( var1, var2, idx )
+function idx =  plotAB( var1, var2, idx, titl )
 
 [ f, var1FileName ] = getFileNameRoot(var1);
 [ f, var2FileName ] = getFileNameRoot(var2);
 
+inPath  = '/Users/cuz/Desktop/Projects/SST/Wind/WindTunnel/2018-02-27/';
+outPath = '/Users/cuz/Desktop/Projects/SST/Wind/WindTunnel/2018-02-27/AB/';
 
-
-var1FName = [ var1FileName, '*.flac' ];
-var2FName = [ var2FileName, '*.flac' ];
+var1FName = [ inPath, var1FileName, '*.flac' ];
+var2FName = [ inPath, var2FileName, '*.flac' ];
 
 fileName1 = dir( var1FName );
-td1 = loadWavTD( fileName1.name );
-
+td1 = loadWavTD( [ inPath, fileName1.name ] );
 fileName2 = dir( var2FName );
-td2 = loadWavTD( fileName2.name );
+td2 = loadWavTD( [ inPath, fileName2.name ] );
 
 fftL = 4096;
 figure;
+%yyaxis left
 plot(spectrum(td1,fftL));
 hold on;
-plot(spectrum(td2,fftL));
+%yyaxis left
+plot(spectrum(td2,fftL),'LineStyle','--');
 set(gca,'XScale','log');
 set(gca,'YScale','log');
+
+%yyaxis right
+%plot( mscohere( td1, td2, 0.75, fftL ), 'Color', [0.8 0.8 0.8] )
+
 aa = get( gca, 'XLim' );
 set( gca, 'XLim', [ aa(1), td1.sampleRate/2 ] );
-title( [ fileName1.name, ' .vs. ', fileName2.name ] );
+title( [ 'A:B = ', titl ] );
 set(gcf, 'OuterPosition', [ 400 500 1200 900 ] )
 set(gca,'FontSize',14)
+%legend( { fileName1.name, fileName2.name, 'coherence' }, 'Interpreter', 'none' )
 legend( { fileName1.name, fileName2.name }, 'Interpreter', 'none' )
-print( gcf,'-djpeg100', [ var1FileName, 'vs_', var2FileName, 'AB.jpg' ] );
-print( gcf,'-dpdf', '-bestfit', [ var1FileName, 'vs_', var2FileName, 'AB.pdf' ] );
+pathRoot = [ outPath, '/', var1FileName, 'vs_', var2FileName ];
+print( gcf,'-djpeg100', [ pathRoot, 'AB.jpg' ] );
+print( gcf,'-dpdf', '-bestfit', [ pathRoot, 'AB.pdf' ] );
 
--- plotVariant.m
+++ plotVariant.m
@@ -1,10 +1,18 @@
-function plotVariant( fRoot )
+function plotVariant( variant )
 
-flacName = [ fRoot, '.flac' ];
-td = loadWavTD( flacName );
+% Load uadio file
+[ f, fRoot ] = getFileNameRoot( variant );
+inPath  = '/Users/cuz/Desktop/Projects/SST/Wind/WindTunnel/2018-02-27/';
+var1FName = [ inPath, fRoot, '*.flac' ];
+fileName1 = dir( var1FName );
+td = loadWavTD( [ inPath, fileName1.name ] );
 
+% Pre output path, group by folder
+group = fRoot(1:2);
+outPath  = [ '/Users/cuz/Desktop/Projects/SST/Wind/WindTunnel/2018-02-27/', group, '/' ];
 
-tsName = [ fRoot, '.ts.jpg' ];
+% Time series
+tsName = [ outPath, fRoot, '.ts.jpg' ];
 plotTitle = td.title;
 figure;
 plot(td);
@@ -15,8 +23,9 @@ set(gcf, 'OuterPosition', [ 400 500 1200 900 ] )
 print( gcf,'-djpeg100', tsName );
 close('all');
 
+% Spectrum
 fftL = 4096 * 4;
-psName = [ fRoot, '.spect.jpg' ];
+psName = [ outPath, fRoot, '.spect.jpg' ];
 plotTitle = [ 'Spectrum of: ', td.title ];
 figure;
 plot(spectrum(td,fftL,1))
@@ -29,8 +38,9 @@ set(gcf, 'OuterPosition', [ 400 500 1200 900 ] )
 print( gcf,'-djpeg100', psName );
 close('all');
 
+% Spectrogram
 fftL = 4096 * 4;
-sgramName = [ fRoot, '.sgram.jpg' ];
+sgramName = [ outPath, fRoot, '.sgram.jpg' ];
 plotTitle = [ 'Spectrogram of: ', td.title ];
 figure;
 plot(log10(spectrogram(td,fftL,0.8675)))
